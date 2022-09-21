99f4dc36e79f644d3d8e4b1fa1404035c369c26f
Charles Lwanga <clwanga1095@gmail.com>
2022-09-21 04:33:38 +0300

    enhancements on the homepage

--- index.php
+++ index.php
@@ -5,9 +5,52 @@ require_once 'db/connection.php';
 
 $result = $crud->getSpecialities();
 ?>
-<br>
-<h4>Registration form</h4>
-<br>
+<div class="banner">
+    <p id="bannerHeader">Managing meetings <br> Made Easy!</p>
+    <p id="supportingText">Use our application to organise your meetings and document everything with zero trouble</p>
+    <p id="services">Our Services</p>
+</div>
+
+<!-- end of banner -->
+<div class="container px-4">
+    <div class="row g-2 mt-5">
+        <div class="col-6 ps-5">
+            <img src="images/meeting.png" alt="meeting image">
+        </div>
+        <div class="col-6 pt-5 ps-5">
+            <div class="card blueCard" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <button type="button" class="btn btn-info"><i class="fa fa-calendar-plus-o" aria-hidden="true"></i> Create Meeting</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row g-2 mt-5">
+        <div class="col-6 pt-5 ps-5">
+            <div class="card orangeCard" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <button type="button" class="btn btn-warning"><i class="fa fa-user-plus" aria-hidden="true"> Register</i></button>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <img src="images/member.png" alt="meeting image">
+        </div>
+    </div>
+</div>
+
+<div class="memberSection">
+
+
+
+</div>
+
 <!-- start -->
 <form class="w-50 p-3" method="post" action="success.php" enctype="multipart/form-data">
     <div class="form-group">
@@ -34,8 +77,8 @@ $result = $crud->getSpecialities();
     <div class="form-group">
         <label for="speciality">Area of Expertise</label>
         <select class="form-control" name="speciality" id="speciality">
-            <?php while ($r = $result->fetch(PDO::FETCH_ASSOC)) {?>
-            <option value="<?php echo $r['speciality_id']?>"><?php echo $r['name']?></option>
+            <?php while ($r = $result->fetch(PDO::FETCH_ASSOC)) { ?>
+                <option value="<?php echo $r['speciality_id'] ?>"><?php echo $r['name'] ?></option>
             <?php } ?>
         </select>
     </div>
